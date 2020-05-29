83ce89f06c22ee4401eb2a92b259b98c13262456
Yagya Chaudhary <mail@bdryagya.com.np>
2020-05-29 16:57:05 +0545

    quiz test

--- helper/quiz_test.go
+++ helper/quiz_test.go
@@ -1,21 +1,83 @@
 package helper
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBuild(t *testing.T) {
 
-	q := Quiz{
-		Question: "A Question",
-		Options: []Option{
-			"First",
-			"Second",
-			"Third",
-			"Correct",
+	q := (Quiz{}).Build(
+		QuizConfig{
+			PerStage: 4,
 		},
-		CorrectOption: 3,
-	}
+		QuizStat{},
+		[]Quiz{
+			Quiz{
+				ID:            1,
+				Question:      "2*3",
+				Options:       []Option{"4", "5", "6", "7"},
+				CorrectOption: 2,
+			},
+			Quiz{
+				ID:            2,
+				Question:      "2+3",
+				Options:       []Option{"4", "5", "6", "7"},
+				CorrectOption: 1,
+			},
+			Quiz{
+				ID:            3,
+				Question:      "2-3",
+				Options:       []Option{"1", "-1", "0", "5"},
+				CorrectOption: 1,
+			},
+			Quiz{
+				ID:            4,
+				Question:      "6/2",
+				Options:       []Option{"1", "2", "3", "4"},
+				CorrectOption: 2,
+			},
+			Quiz{
+				ID:            5,
+				Question:      "123-1",
+				Options:       []Option{"122", "123", "125", "129"},
+				CorrectOption: 0,
+			},
+		})
 
-	if q.IsCorrect(4) != true {
+	if q.IsCorrect(3) != true {
 		t.Errorf("Failed to verify ans. got %T, want true", q.IsCorrect(3))
 	}
+
+	t.Run("Chain", func(t *testing.T) {
+
+		if q.Next.Prev != q {
+			t.Errorf("Next's Prev is not Current")
+		}
+
+		if q.Prev.Next != q {
+			t.Errorf("Prev's Next is not Currect")
+		}
+	})
+
+	t.Run("Advancing", func(t *testing.T) {
+		n := q.Advance()
+
+		if q.Next != n {
+			t.Errorf("Currect quiz is not Next of old quiz")
+		}
+
+	})
+
+	t.Run("Masking", func(t *testing.T) {
+		q.Mask()
+
+		// removes current quiz from chain
+		if q.Prev.Next != q.Next {
+			t.Errorf("Prev's Next is not Next")
+		}
+
+		if q.Next.Prev != q.Prev {
+			t.Errorf("Next's Prev is not Prev")
+		}
+	})
 }
